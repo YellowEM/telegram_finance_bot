f02fbb5a751bbf392b0441c972ef64eee5e7989b
YellowEM <drgnom95@gmail.com>
2023-07-22 13:48:37 +0300

    upping categories(add return, guid) up db fetchall

--- db.py
+++ db.py
@@ -8,12 +8,25 @@ cursor = conn.cursor()
 
 
 def insert(table: str, column_values: Dict):
-    columns = ', '.join( column_values.keys() )
+    columns = ', '.join(column_values.keys())
     values = [tuple(column_values.values())]
-    placeholders = ", ".join( "?" * len(column_values.keys()) )
+    placeholders = ", ".join("?" * len(column_values.keys()))
     cursor.executemany(
         f"INSERT INTO {table} "
         f"({columns}) "
         f"VALUES ({placeholders})",
         values)
     conn.commit()
+
+
+def fetchall(table: str, columns: List[str]) -> List[Tuple]:
+    columns_joined = ", ".join(columns)
+    cursor.execute(f"SELECT {columns_joined} FROM {table}")
+    rows = cursor.fetchall()
+    result = []
+    for row in rows:
+        dict_row = {}
+        for index, column in enumerate(columns):
+            dict_row[column] = row[index]
+        result.append(dict_row)
+    return result
