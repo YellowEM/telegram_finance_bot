ad8c96e71b0c3ded9834a5392b29c48dcefee0ba
YellowEM <drgnom95@gmail.com>
2023-07-23 09:38:16 +0300

    add returns purchases

--- expenses.py
+++ expenses.py
@@ -4,6 +4,9 @@ import re
 from categories import Categories
 import db
 import exceptions
+from typing import List, NamedTuple, Optional
+import pytz
+
 
 class Message(NamedTuple):
     amount: int
@@ -15,7 +18,8 @@ class Expense(NamedTuple):
     amount: int
     category_name: str
 
-def add_expense(row_message:str) -> Expense:
+
+def add_expense(raw_message: str) -> Expense:
     parsed_message = _parse_message(raw_message)
     category = Categories().get_category(
         parsed_message.category_text)
@@ -29,6 +33,7 @@ def add_expense(row_message:str) -> Expense:
                    amount=parsed_message.amount,
                    category_name=category.name)
 
+
 def _parse_message(raw_message: str) -> Message:
     """Парсит вновь введённый текст"""
     regexp_result = re.match(r"([\d]+)(.*)", raw_message)
@@ -37,3 +42,19 @@ def _parse_message(raw_message: str) -> Message:
     amount = regexp_result.group(1).replace(" ", "")
     category_text = regexp_result.group(2).strip().lower()
     return Message(amount=amount, category_text=category_text)
+
+
+def _get_now_formatted() -> str:
+    """Возвращает сегодняшнюю дату строкой"""
+    return _get_now_datetime().strftime("%Y-%m-%d %H:%M:%S")
+
+def _get_now_datetime() -> datetime.datetime:
+    """Возвращает сегодняшний datetime с учётом временной зоны Мск."""
+    tz = pytz.timezone("Europe/Moscow")
+    now = datetime.datetime.now(tz)
+    return now
+
+
+def _get_budget_limit() -> int:
+    """Возвращает дневной лимит трат для основных базовых трат"""
+    return db.fetchall("budget", ["daily_limit"])[0]["daily_limit"]
