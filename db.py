e9274c6f881556ef918511e8e9805e7fff2fb540
YellowEM <drgnom95@gmail.com>
2023-07-23 09:27:56 +0300

    ending db

--- db.py
+++ db.py
@@ -30,5 +30,34 @@ def fetchall(table: str, columns: List[str]) -> List[Tuple]:
             dict_row[column] = row[index]
         result.append(dict_row)
     return result
-=======
->>>>>>> origin/dev
+
+
+def delete(table: str, row_id: int) -> None:
+    row_id = int(row_id)
+    cursor.execute(f"delete from {table} where id={row_id}")
+    conn.commit()
+
+
+def get_cursor():
+    return cursor
+
+
+def _init_db():
+    """Инициализирует базу данных"""
+    with open("createdb.sql", "r") as f:
+        sql = f.read()
+    cursor.executescript(sql)
+    conn.commit()
+
+
+def check_db_exists():
+    """Проверяет, инициализирована ли БД, если нет — инициализирует"""
+    cursor.execute("SELECT name FROM sqlite_master "
+                   "WHERE type='table' AND name='expense'")
+    table_exists = cursor.fetchall()
+    if table_exists:
+        return
+    _init_db()
+
+
+check_db_exists()
